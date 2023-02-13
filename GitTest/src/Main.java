8bbe6b46c74ee79c0909a2d37c2073ec42bd652d
Barna Udvarhelyi <barna.udvarhelyi@gmail.com>
2023-02-13 14:21:16 +0100

    a2 fixed

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -1,17 +1,11 @@
 public class Main {
-
-<<<<<<< HEAD
-    public static void greet(String name) {
-        System.out.println(name);
-    }
-=======
+    
   public static void main(String[] args) {
     System.out.println("Hello world!");
   }
->>>>>>> afc0f89aa919885928a0b53e964935ad2d1d548d
 
-  public static void greet() {
-    System.out.println("Barna Udvarhelyi.");
+  public static void greet(String name) {
+      System.out.println(name);
   }
 
   public static void cheer() {
