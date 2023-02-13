8f1f4f3b4a66967674f40fddfb7a741e862c2d08
Lukas Janosik <lukasjanosiks4@gmail.com>
2023-02-13 15:33:39 +0100

    d5

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -10,25 +10,20 @@ public class Main {
     System.out.println(name);
   }
 
-<<<<<<< HEAD
   public static void horoscope(String scorpion, Integer age) {
     System.out.println(scorpion + age + "");
-=======
-  public static void horoscope() {
-    System.out.println("Rat");
->>>>>>> c66d135121a8176b52eab790146e6da3f8acf768
   }
+
 
   public static void cheer() {
     String greetingMessage = "FoxBuy.";
     System.out.println(greetingMessage);
-<<<<<<< HEAD
+
   }
 
 
   public static void count(int age) {
     System.out.println(age + " years");
-=======
     System.out.println("Slovak language");
   }
 
@@ -36,6 +31,5 @@ public class Main {
   public static void count(int age, String developerName) {
     System.out.println(age + " years");
     System.out.println(developerName + age);
->>>>>>> c66d135121a8176b52eab790146e6da3f8acf768
   }
 }
