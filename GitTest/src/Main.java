57a24c572a1673d724631c56839444384d60de5b
Lukas Janosik <lukasjanosiks4@gmail.com>
2023-02-13 14:34:20 +0100

    d4

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -1,31 +1,24 @@
 public class Main {
 
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
     public static void greet(String name) {
         System.out.println(name);
     }
 
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-
-
-  public static void greet() {
-    System.out.println("Barna Udvarhelyi.");
-  }
-
     public static void horoscope() {
         System.out.println("Scorpion and something");
     }
-    public static void count() {
-        System.out.println("20 years");
+
+    public static void cheer() {
+        System.out.println("FoxBuy.");
     }
-  public static void cheer() {
-    System.out.println("FoxBuy.");
-  }
 
-
-
-  public static void count(int age) {
-    System.out.println(age + " years");
-  }
+    public static void count(int age) {
+        System.out.println(age + " years");
+    }
 }
