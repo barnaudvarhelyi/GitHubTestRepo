c9c4cd872b8db47e9b3d3bb31af67a9e7f422cf1
Lukas Janosik <lukasjanosiks4@gmail.com>
2023-02-13 15:14:14 +0100

    d5

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -1,26 +1,26 @@
 public class Main {
 
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 
 
-    public static void greet(String name) {
-        System.out.println(name);
-    }
+  public static void greet(String name) {
+    System.out.println(name);
+  }
 
-    public static void horoscope() {
-        System.out.println("Scorpion and something");
-    }
+  public static void horoscope(String scorpion, Integer age) {
+    System.out.println(scorpion + age);
+  }
 
-    public static void cheer() {
-        String greetingMessage = "FoxBuy.";
-        System.out.println(greetingMessage);
-    }
+  public static void cheer() {
+    String greetingMessage = "FoxBuy.";
+    System.out.println(greetingMessage);
+  }
 
 
-    public static void count(int age) {
-        System.out.println(age + " years");
-    }
+  public static void count(int age) {
+    System.out.println(age + " years");
+  }
 }
