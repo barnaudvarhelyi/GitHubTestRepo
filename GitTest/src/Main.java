afc0f89aa919885928a0b53e964935ad2d1d548d
Martin Maťovčík <matko1vcik@gmail.com>
2023-02-13 14:19:16 +0100

    b2

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -1,20 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
-    public static void greet() {
-        System.out.println("Barna Udvarhelyi.");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 
-    public static void cheer() {
-        System.out.println("FoxBuy.");
-    }
+  public static void greet() {
+    System.out.println("Barna Udvarhelyi.");
+  }
 
-    public static void horoscope() {
-        System.out.println("Scorpion");
-    }
-    public static void count() {
-        System.out.println("20 years");
-    }
+  public static void cheer() {
+    System.out.println("FoxBuy.");
+  }
+
+  public static void horoscope() {
+    System.out.println("Scorpion");
+  }
+
+  public static void count(int age) {
+    System.out.println(age + " years");
+  }
 }
