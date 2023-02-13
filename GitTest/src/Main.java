bf9833d7800db5a0cd1cd6e1480c298fccdb3840
Martin Maťovčík <matko1vcik@gmail.com>
2023-02-13 14:09:07 +0100

    b1

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -6,4 +6,7 @@ public class Main {
     public static void greet() {
         System.out.println("Barna Udvarhelyi.");
     }
+    public static void count() {
+        System.out.println("20 years");
+    }
 }
